ae9517e880d35a72d9fd9609cec51f34a890d2d2
Dominik Wikliński <domel2222@gmail.com>
2021-02-18 17:45:32 +0100

    added  model classes

---
+++ Models/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premier.Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+        public Tournament Tournament { get; set; }
+
+    }
+}
