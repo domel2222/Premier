c9783f37f9f771b9cb73d7463df661c56f9ffed3
Dominik Wikliński <domel2222@gmail.com>
2021-02-20 16:41:15 +0100

    created tournament repository interface
    added method for tournament and general

--- DAL/ITournamentRepository.cs
+++ DAL/ITournamentRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Premier.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,23 @@ namespace Premier.DAL
 {
     public interface ITournamentRepository
     {
-        
+        //general 
+
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+        Task<bool> SaveChangesAsync();
+
+
+        //Tournament
+
+        Task<Tournament[]> GetAllTournamentAsync(bool includeMatches = false);
+        Task<Tournament> GetTournamentAsync(string nickName, bool includeMatche = false);
+        Task<Tournament[]> GetAllTournamentByEventData(DateTime dateTime, bool includeMatches = false);
+
+        //Maches
+
+
+        //Teams
     }
 }
