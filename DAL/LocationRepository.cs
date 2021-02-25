ffc835c239e215b346d39eb4b4959a26bc9cc106
Dominik Wikliński <domel2222@gmail.com>
2021-02-25 12:13:38 +0100

    added body to LoctionRepository GetAllLoction GetLocationById SvreChanges

--- DAL/LocationRepository.cs
+++ DAL/LocationRepository.cs
@@ -1,4 +1,7 @@
-﻿using Premier.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Premier.Context;
+using Premier.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +12,39 @@ namespace Premier.DAL
 {
     public class LocationRepository : ILocationRepository
     {
-        public Task<Location[]> GetAllLocation()
+        private readonly TournamentContext _context;
+        private readonly ILogger _logger;
+
+        public LocationRepository(TournamentContext context, ILogger logger)
         {
-            throw new NotImplementedException();
+            this._context = context;
+            this._logger = logger;
+        }
+        public async Task<Location[]> GetAllLocation()
+        {
+            _logger.LogInformation($"Getting all location");
+
+            var query = _context.Locations.OrderBy(l => l.LocationId);
+
+            return await query.ToArrayAsync();
         }
 
-        public Task<Location> GetLocationById(int locationId)
+
+        public async Task<Location> GetLocationById(int locationId)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"Getting location by {locationId}");
+
+            var query = _context.Locations.Where(l => l.LocationId == locationId);
+
+            return await query.FirstOrDefaultAsync();
         }
 
-        public Task<bool> SaveChangesAsync()
+
+        public async Task<bool> SaveChangesAsync()
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"Attempting to save this change");
+
+            return (await _context.SaveChangesAsync() > 0);
         }
     }
 }
