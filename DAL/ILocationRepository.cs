b4786be2851424c89a268e1b242151a625c061ec
Dominik Wikliński <domel2222@gmail.com>
2021-02-25 11:57:53 +0100

    crted ILocationRepository

---
+++ DAL/ILocationRepository.cs
@@ -0,0 +1,22 @@
+﻿using Premier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premier.DAL
+{
+    public interface ILocationRepository
+    {
+
+
+        Task<bool> SaveChangesAsync();
+
+        //Location 
+        Task<Location[]> GetAllLocation();
+
+        Task<Location> GetLocationById(int locationId);
+
+    }
+}
---
+++ DAL/LocationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Premier.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Premier.DAL
+{
+    public class LocationRepository : ILocationRepository
+    {
+        public Task<Location[]> GetAllLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Location> GetLocationById(int locationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
